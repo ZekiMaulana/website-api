024e1393a59d314692b2df10821caac298d9c186
Z E K I <zeki.maulana.rahman@gmail.com>
2025-03-18 17:24:07 +0700

    add readme and modified design a little

--- index.js
+++ index.js
@@ -27,41 +27,38 @@ function readJson(fileName) {
     return JSON.parse(file)
 }
 
-const resultTopAired = readJson("topAiredTv");
-const resultTopAiredMovie = readJson("topAiredMovie");
+// use data JSON local
 const resultGenres = readJson("genres")
 const resultExplicitGenres = readJson("explicit_genres")
 const resultChar = readJson("topCharacters")
 const seasonsList = readJson("seasonsList")
-const resultTopAnime = readJson("topAnime")
-const resultTopUpcoming = readJson("topUpcoming")
+
 
 app.get("/", async (req, res) => {
     try {
 
         // use public api
 
-        // const paramsTopAired = { filter: "airing", type: "tv"};
-        // const resultTopAired = await axios(API_URL + "top/anime", {params: paramsTopAired});
+        const paramsTopAired = { filter: "airing", type: "tv"};
+        const resultTopAired = await axios(API_URL + "top/anime", {params: paramsTopAired});
 
-        // const paramsTopAiredMovie = { type: "movie"};
-        // const resultTopAiredMovie = await axios(API_URL + "top/anime", {params: paramsTopAiredMovie});
+        const paramsTopAiredMovie = { type: "movie"};
+        const resultTopAiredMovie = await axios(API_URL + "top/anime", {params: paramsTopAiredMovie});
 
-        // const paramsTopUpcoming = { filter: "upcoming"};
-        // const resultTopUpcoming = await axios(API_URL + "top/anime", {params: paramsTopUpcoming});
+        const paramsTopUpcoming = { filter: "upcoming"};
+        const resultTopUpcoming = await axios(API_URL + "top/anime", {params: paramsTopUpcoming});
 
-        // const paramsTopAnime = { type: "tv"};
-        // const resultTopAnime = await axios(API_URL + "top/anime", {params: paramsTopAnime});
+        const paramsTopAnime = { };
+        const resultTopAnime = await axios(API_URL + "top/anime", {params: paramsTopAnime});
         
-        // res.render("index.ejs", {content: resultTopAired.data,  characters: resultChar movie: resultTopAiredMovie.data, top: resultTopAnime.data});
+        res.render("index.ejs", {content: resultTopAired.data,  characters: resultChar, movie: resultTopAiredMovie.data, top: resultTopAnime.data, upcoming: resultTopUpcoming.data});
 
-        // use dataJSON
+        
               
-        res.render("index.ejs", {content: resultTopAired, characters: resultChar, movie: resultTopAiredMovie, top: resultTopAnime, upcoming: resultTopUpcoming});
 
     } catch (error) {
         console.error("Failed to make request:", error.message);
-        res.render("index.ejs", { error: error.message });
+        res.render("error.ejs", { error: error.message });
     }
 });
 
@@ -73,7 +70,7 @@ app.get("/anime", async (req, res) => {
         
         res.render("content.ejs", {content: result.data, characters: resultChar, episodes: resultEpisodes.data})
     } catch(error) {
-        res.render("content.ejs", { error: error.message })
+        res.render("error.ejs", { error: error.message })
     }
 });
 
@@ -85,7 +82,7 @@ app.get("/category", async (req, res) => {
                 res.render("category.ejs", {category: resultGenres, characters: resultChar, categoryE: resultExplicitGenres});
             }catch (error) {
                 console.error("Failed to make request:", error.message);
-                res.render("category.ejs", { error: error.message });
+                res.render("error.ejs", { error: error.message });
             }
             break;
         case "release-year":
@@ -94,13 +91,15 @@ app.get("/category", async (req, res) => {
                 res.render("category.ejs", {years: seasonsList, characters: resultChar})
             } catch(error) {
                 console.error("Failed to make request:", error.message);
-                res.render("category.ejs", { error: error.message });
+                res.render("error.ejs", { error: error.message });
             }
             break;
         default:
             break;
     }
 });
+
+
 
 app.get("/search/genres", async (req, res) => {
     try {
@@ -110,34 +109,73 @@ app.get("/search/genres", async (req, res) => {
 
         res.render("search.ejs", {content: result.data, characters: resultChar})
     }catch(error) {
-        res.render("search.ejs", {error: error.message})
+        res.render("error.ejs", {error: error.message})
     }
 })
 
 app.post("/search", async(req, res) => {
     try {
-        const genreId = req.body.genreId
-        console.log(req.body.genreId)
-        res.locals.nameGenre = req.body.name
-        res.locals.year = req.body.year
         var start_date = ""
         var end_date = ""
         var genreX = 12
-        if (req.body.year){
-        start_date = req.body.year + "-01-01"
-        end_date = (req.body.year) + "-12-31"
-        console.log(end_date)
-        }
-        if (genreId == 12){
-            genreX = 0
-        }
-        const params = {order_by: "score", sort: "desc", genres: genreId, genres_exclude: genreX,  start_date: start_date, end_date: end_date, page: 2 }
-        const result = await axios.get(API_URL + "anime", {params: params})
+        var genreId = ""
+        var page1 = 1
+        var page2 = 2
+        var limit = 21
 
-        res.render("search.ejs", {content: result.data, characters: resultChar})
+        if (req.body.genreId){
+           genreId = req.body.genreId
+           res.locals.nameGenre = req.body.name
+           if (genreId == 12){
+            genreX = 0;
+            }
+           console.log("genreId = "+ genreId)
+        }
+
+        if (req.body.year){
+            res.locals.year = req.body.year
+            start_date = req.body.year + "-01-01"
+            end_date = (req.body.year) + "-12-31"
+            console.log(end_date + " | " + start_date)
+        }
+
+        if (req.body.page){
+            page1 = page1 * 2
+            page2 = page1 - 1
+
+            console.log("page " + page1 + " and " + page2)
+        }
+        
+        const params1 = {
+            order_by: "score", 
+            sort: "desc", 
+            genres: genreId, 
+            genres_exclude: genreX,  
+            start_date: start_date, 
+            end_date: end_date, 
+            page: page1, 
+            limit: limit }
+
+        const result1 = await axios.get(API_URL + "anime", {params: params1})
+
+        const params2 = {
+            order_by: "score", 
+            sort: "desc", 
+            genres: genreId, 
+            genres_exclude: genreX,  
+            start_date: start_date, 
+            end_date: end_date, 
+            page: page2, 
+            limit: limit }
+
+        const result2 = await axios.get(API_URL + "anime", {params: params2})
+
+        res.render("search.ejs", {content1: result1.data, content2: result2.data, characters: resultChar})
     }catch(error) {
-        res.render("search.ejs", {error: error.message})
+        res.render("error.ejs", {error: error.message})
     }
+
+
 
 })
 app.listen(port, () => {
---
+++ readme.md
@@ -0,0 +1,5 @@
+Quick Setup for this project
+
+This project using HTML, EJS for templating, and CSS for design(still struggle with css but i'm trying my best to implementing what i learn). Use JIKAN API and MyAnimeList for data information.
+
+all you need to start this website is install package using "npm i" on terminal in this repo
